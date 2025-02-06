6abf716832b9cdba0812d0da2bff9e2783c41bf6
FlamestarRS <FlamestarRS@gmail.com>
2025-02-05 22:46:09 -0500

    added LeafNode child class and unittests

--- src/htmlnode.py
+++ src/htmlnode.py
@@ -18,5 +18,22 @@ class HTMLNode:
 
         return all_props_str
     
-    def __repr__ (self):
+    def __repr__(self):
         return f"HTMLNode({self.tag}, {self.value}, {self.children}, {self.props})"
+    
+class LeafNode(HTMLNode):
+    def __init__(self, tag=None, value=None, props=None):
+        super().__init__(tag, value, children=None, props=props)
+
+    def to_html(self):
+        if self.value == None:
+            raise ValueError
+        if self.tag == None:
+            return str(self.value)
+        if self.props == None:
+            return f"<{self.tag}>{self.value}</{self.tag}>"
+        htmlprops = super().props_to_html()
+        return f"<{self.tag}{htmlprops}>{self.value}</{self.tag}>"
+    
+    def __repr__(self):
+        return f"LeafNode({self.tag}, {self.value}, {self.props})"
--- src/test_htmlnode.py
+++ src/test_htmlnode.py
@@ -1,6 +1,6 @@
 import unittest
 
-from htmlnode import HTMLNode
+from htmlnode import HTMLNode, LeafNode
 
 class TestHTMLNode(unittest.TestCase):
     def test_repr(self):
@@ -20,6 +20,20 @@ class TestHTMLNode(unittest.TestCase):
     def test_to_html(self):
         htmlnode = HTMLNode()
         self.assertRaises(NotImplementedError, htmlnode.to_html)
+
+class TestLeafNode(unittest.TestCase):
+    def test_to_html_without_props(self):
+        leafnode = LeafNode("p", "normal text")
+        self.assertEqual(leafnode.to_html(), "<p>normal text</p>")
+
+    def test_to_html_with_props(self):
+        dict = {"href":"weesnaw"}
+        leafnode = LeafNode("p", "normal text", dict)
+        self.assertEqual(leafnode.to_html(), '<p href="weesnaw">normal text</p>')
+
+    def test_repr(self):
+        leafnode = LeafNode("tag", "value", "props")
+        self.assertEqual(repr(leafnode), "LeafNode(tag, value, props)")
 
 
 if __name__ == "__main__":
